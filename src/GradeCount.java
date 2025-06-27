1bf61a11d6902c1f390c9629612c7624c8585ebe
Lekhana B M <lekhanalekhana059@gmail.com>
2025-06-27 19:40:03 +0530

    Initial commit

---
+++ src/GradeCount.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GradeCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int D_count = 0;
+        int C_count = 0;
+        int B_count = 0;
+        int A_count = 0;
+
+        for(int i = 1; i <= n; i++){
+            char grade = sc.next().charAt(0);
+            if(grade=='D'){
+                D_count++;
+            }
+            if(grade=='C'){
+                C_count++;
+            }
+            if(grade=='B'){
+                B_count++;
+            }
+            if(grade=='A') {
+                A_count++;
+            }
+        }
+        System.out.println((D_count+" "+C_count+" "+B_count+" "+A_count));
+    }
+}
